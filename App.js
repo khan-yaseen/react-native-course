e9c4fc4af8f0211a5139bb5746ea5704d494ec88
yaseen-khan <yaseenkhan61@yahoo.com>
2024-03-08 18:18:05 +0530

    styling the list of goals

--- App.js
+++ App.js
@@ -21,7 +21,11 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {goals.map((goal) => <Text key={goal}>{goal}</Text> )}
+        {goals.map((goal) => (
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -51,5 +55,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
   }
 });
