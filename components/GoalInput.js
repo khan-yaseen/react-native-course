5001955a3aa13b6cc59f7106277e99f9f1fb1741
yaseen-khan <yaseenkhan61@yahoo.com>
2024-03-09 01:21:30 +0530

    finishing touches

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,10 +25,10 @@ function GoalInput(props) {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title='Add Goal' onPress={addGoalHandler} />
+                        <Button title='Cancel' onPress={props.onCancel} color="#b180f0" />
                     </View>
                     <View style={styles.button}>
-                        <Button title='Cancel' onPress={props.onCancel} />
+                        <Button title='Add Goal' onPress={addGoalHandler} color="#f31282" />
                     </View>
                 </View>
             </View>
@@ -53,9 +53,11 @@ const styles = StyleSheet.create({
     },
     textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
-        padding: 8,
-        margin: 8,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
+        padding: 16,
         width: '100%'
     },
     buttonContainer: {
